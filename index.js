897e9354bb4da0c5c8be878c4f3eb1513df925bf
Oxana Rozhkova <far_north@ro.ru>
2023-09-20 21:44:30 +0300

    fonts + timer

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//таймер
+(function () {
+	let limit = new Date(2023, 9, 21, 21, 24, 0, 0);
+	let input = document.querySelector('.timer');
+
+	setInterval(() => {
+		let now = new Date();
+		let result = limit.getTime() - now.getTime();
+		let resultDate = new Date(result);
+		input.textContent = `${
+			resultDate.getUTCHours() < 10
+				? `0${resultDate.getUTCHours()}`
+				: `${resultDate.getUTCHours()}`
+		}:${
+			resultDate.getUTCMinutes() < 10
+				? `0${resultDate.getUTCMinutes()}`
+				: `${resultDate.getUTCMinutes()}`
+		}:${
+			resultDate.getUTCSeconds() < 10
+				? `0${resultDate.getUTCSeconds()}`
+				: `${resultDate.getUTCSeconds()}`
+		}`;
+	}, 1000);
+})();
